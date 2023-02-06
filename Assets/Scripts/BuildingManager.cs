285abb9c76b201e16ed3883e1a3390b5859592ec
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2023-02-06 16:25:25 +0300

    Confetti

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -16,7 +16,8 @@ if(BuildingIndex == PlayerFreeFallManager.Instance.targetBuilding)
     PlayerManager.Instance.environmentMoveScript.enabled = false;
     PlayerManager.Instance.fallMoveScript.enabled=false;
     PlayerManager.Instance.myAnimator.SetBool("Ending",true);
-    
+    PlayerManager.Instance.transform.GetChild(PlayerManager.Instance.transform.childCount-1).gameObject.SetActive(true);
+
 }
 
 }
