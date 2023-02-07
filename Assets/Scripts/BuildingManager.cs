40c8a01ea28d0660b48d9acbe3287410157ef038
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2023-02-07 11:01:03 +0300

    Landing & Confetti

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BuildingManager : MonoBehaviour
 {
    public GameObject BuildingIndex;
- 
+    private ParticleSystem confetti;
 
-
+    private void Start() {
+        BuildingIndex.transform.GetChild(0).transform.gameObject.SetActive(false);
+    }
 
 public void CheckWin(){
 if(BuildingIndex == PlayerFreeFallManager.Instance.targetBuilding)
 {
-    Debug.Log("bitti");
+        confetti = BuildingIndex.transform.GetChild(0).GetComponent<ParticleSystem>();
+
+
     PlayerManager.Instance.environmentMoveScript.enabled = false;
     PlayerManager.Instance.fallMoveScript.enabled=false;
     PlayerManager.Instance.myAnimator.SetBool("Ending",true);
-    PlayerManager.Instance.transform.GetChild(PlayerManager.Instance.transform.childCount-1).gameObject.SetActive(true);
+PlayerManager.Instance.agent.transform.GetChild(3).transform.gameObject.SetActive(false);
+    
+    PlayerManager.Instance.agent.transform.DORotate(new Vector3(0,0,0),2f).OnComplete(()=>{
+PlayerManager.Instance.agent.transform.GetChild(0).transform.gameObject.SetActive(false);
+confetti.Play();
+PlayerManager.Instance.transform.GetChild(PlayerManager.Instance.transform.childCount-1).gameObject.SetActive(true);
+    });
 
 }
 
